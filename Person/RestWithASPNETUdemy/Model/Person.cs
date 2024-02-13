56ddd74981824b3229e87473913514e6e75ae67b
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-02-13 16:52:22 -0300

    Criando a aplicação sem os Mocks e conetando ao DB

--- Person/RestWithASPNETUdemy/Model/Person.cs
+++ Person/RestWithASPNETUdemy/Model/Person.cs
@@ -1,11 +1,19 @@
-﻿namespace RestWithASPNETUdemy.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
 {
+    [Table("person")]//referenciando o nome da tabela que será usada no db
     public class Person
     {
+        [Column("id")]//referenciando o nome da coluna que será usada no db
         public long Id { get; set; }
+        [Column("first_name")]
         public string FirstName{ get; set; }
+        [Column("last_name")]
         public string LastName{ get; set; }
+        [Column("address")]
         public string Address{ get; set; }
+        [Column("gender")]
         public string Gender{ get; set; }    
     }
 }
