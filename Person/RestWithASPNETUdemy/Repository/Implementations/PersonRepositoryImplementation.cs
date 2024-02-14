ad31feb930675dfebc7d020d0a191b77a313b255
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-02-14 17:44:06 -0300

    Adicionando as classes migrations e seeding

--- Person/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
+++ Person/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -13,19 +13,16 @@ namespace RestWithASPNETUdemy.Repository.Implementations
             _context = context;
         }
 
-        // Method responsible for returning all people,
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
         }
 
-        // Method responsible for returning one person by ID
         public Person FindById(long id)
         {
             return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
-        // Method responsible to crete one new person
         public Person Create(Person person)
         {
             try
@@ -40,20 +37,15 @@ namespace RestWithASPNETUdemy.Repository.Implementations
             return person;
         }
 
-        // Method responsible for updating one person
         public Person Update(Person person)
         {
-            // We check if the person exists in the database
-            // If it doesn't exist we return an empty person instance
-            if (!Exists(person.Id)) return new Person();
+            if (!Exists(person.Id)) return null;
 
-            // Get the current status of the record in the database
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
             if (result != null)
             {
                 try
                 {
-                    // set changes and save
                     _context.Entry(result).CurrentValues.SetValues(person);
                     _context.SaveChanges();
                 }
@@ -65,7 +57,6 @@ namespace RestWithASPNETUdemy.Repository.Implementations
             return person;
         }
 
-        // Method responsible for deleting a person from an ID
         public void Delete(long id)
         {
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
