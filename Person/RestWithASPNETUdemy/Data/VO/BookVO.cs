cddd91ea80e7908eb8c898527a94fc4e63f241f2
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-02-21 08:37:44 -0300

    Implementando HATEOAS

--- Person/RestWithASPNETUdemy/Data/VO/BookVO.cs
+++ Person/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
-    public class BookVO
+    public class BookVO : ISupportsHyperMedia
     {
         public long Id { get; set; }
         public DateTime Launch_Date { get; set; }
         public string Author { get; set; }
         public decimal Price { get; set; }
         public string Title { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
     }
 }
 
